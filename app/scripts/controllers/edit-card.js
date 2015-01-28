9a7efcece9a430470f9555ce107eebd4d05579b0
Nate Hayflick <nhayflick@gmail.com>
2015-01-28 08:50:23 -0800

    Add users to edit card dialog

--- app/scripts/controllers/edit-card.js
+++ app/scripts/controllers/edit-card.js
@@ -8,15 +8,34 @@
  * Controller of the angularFirebaseTrelloApp
  */
 'use strict';
-app.controller('EditCardCtrl', function ($scope, $mdDialog, $firebase, $mdToast, Authenticate, card, list, lists) {
+app.controller('EditCardCtrl', function ($scope, $mdDialog, $firebase, FIREBASE_URL, $mdToast, Card, User, card, list, lists) {
     var list = list;
     $scope.card = card;
+    $scope.users = User.all();
       // Create a task card and add it to a list
     $scope.updateCard = function () {
       list.cards.$save(card)
         .then( function () {
+          card.users = Card.users(card.$id);
           $mdToast.show($mdToast.simple().position('bottom right').content('Card Updates: ' + $scope.card.name));
           $mdDialog.hide()
         });
+    };
+    $scope.addUserToCard = function (user, card) {
+      var ref = new Firebase(FIREBASE_URL + '/user_cards/' + user.$id + '/' + card.$id);
+      var sync = $firebase(ref);
+      // Uniqueness validated here on backend
+      sync.$set(true).then(function(){
+        // if object was validated unique in user_cards then add to card.users
+        var ref = new Firebase(FIREBASE_URL + '/card/' + card.$id + '/users');
+        var sync = $firebase(ref);
+        var content = {};
+        var userObject = {
+          name: user.name,
+          md5_hash: user.md5_hash
+        };
+        content[user.$id] = userObject;
+        sync.$update(content);
+      });
     };
   });
